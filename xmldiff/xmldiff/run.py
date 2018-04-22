093764cfa72e99bf75291981f26adad29ae7591b
jimsch <ietf@augustcellars.com>
2018-04-22 15:03:57 -0700

    Revert "Rename to match the top name"
    This reverts commit a9c23935b309344b47e6a036b1163770efc1e393.

--- xmldiff/xmldiff/run.py
+++ xmldiff/xmldiff/run.py
@@ -8,12 +8,12 @@ import six
 import sys
 from rfctools_common.parser import XmlRfc, XmlRfcParser, XmlRfcError, CACHES
 from rfctools_common import log
-from rfc-xmldiff.DiffNode import DiffRoot, BuildDiffTree, DecorateSourceFile, AddParagraphs, tagMatching
+from xmldiff.DiffNode import DiffRoot, BuildDiffTree, DecorateSourceFile, AddParagraphs, tagMatching
 import string
-from rfc-xmldiff.EditItem import EditItem
-from rfc-xmldiff.zzs2 import distance
+from xmldiff.EditItem import EditItem
+from xmldiff.zzs2 import distance
 # from xmldiff.zzs import distance, EditItem
-from rfc-xmldiff.__init__ import __version__
+from xmldiff.__init__ import __version__
 
 try:
     import debug
