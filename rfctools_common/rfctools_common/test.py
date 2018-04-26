6e4880932ae47b6e00ab9d5cd5ac552f2d1f9d9f
jimsch <ietf@augustcellars.com>
2018-04-25 19:55:06 -0700

    Apparently updating got new errors

--- rfctools_common/rfctools_common/test.py
+++ rfctools_common/rfctools_common/test.py
@@ -13,7 +13,8 @@ class TestParserMethods(unittest.TestCase):
         pep8style = pycodestyle.StyleGuide(quiet=False, config_file="pycode.cfg")
         result = pep8style.check_files(['parser.py', 'log.py', 'utils.py',
                                         'test.py'])
-        self.assertEqual(result.total_errors, 80,
+        print ("Error count is {0}".format(result.total_errors))
+        self.assertEqual(result.total_errors, 114,
                          "Found code style errors (and warnings).")
 
     def test_simple(self):
