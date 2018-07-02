ba63bea6f754517363901420dace27d21a1c7430
jimsch <ietf@augustcellars.com>
2018-07-02 03:44:36 -0700

    Lint XML fragments
    Missed getting the code to check XML lint correctly.  Now implemented w/ test cases.

--- rfclint/rfclint/run.py
+++ rfclint/rfclint/run.py
@@ -10,6 +10,7 @@ import os
 import lxml.etree
 import datetime
 import appdirs
+import six
 from rfctools_common.parser import XmlRfc, XmlRfcParser, XmlRfcError, CACHES, CachingResolver
 from rfctools_common import log
 from rfclint.config import ConfigFile
@@ -243,22 +244,24 @@ def main():
     codeItems = xmlrfc.tree.getroot().xpath("//sourcecode[@type='xml']")
     if len(codeItems) > 0:
         log.note("Validating XML fragments in sourcecode elements")
-        parser = lxml.etree.XMLParser(dtd_validation=False, load_dtd=False, no_network=True,
-                                      resolve_entities=False, recover=True)
         # resolver without knowledge of rfc_number:
         caching_resolver = CachingResolver(no_network=True,
                                            verbose=options.verbose,
-                                           quiet=options.quiet
-        )
-        parser.resolvers.add(caching_resolver)
+                                           quiet=options.quiet)
 
         for item in codeItems:
+            parser = lxml.etree.XMLParser(dtd_validation=False, load_dtd=False, no_network=True,
+                                          resolve_entities=False, recover=False)
+            parser.resolvers.add(caching_resolver)
             try:
-                lxml.etree.fromstring(item.text, parser)
+                file = six.BytesIO(item.text.encode('utf-8'))
+
+                bbb = lxml.etree.parse(file, parser)
+                aaa = bbb
             except (lxml.etree.XMLSyntaxError) as e:
-                log.warn('XML in sourcecode not well formed', e.msg, where=item)
+                log.warn(u'XML in sourcecode not well formed: ', e.msg, where=item)
             except Exception as e:
-                log.warn('Error occured processing XML', e.msg, where=item)
+                log.warn(u'Error occured processing XML: ', e, where=item)
     else:
         log.note("No XML fragments in sourcecode elements found.")
 
@@ -307,7 +310,7 @@ def main():
             raise
 
     if options.output_filename is not None:
-        file = open(options.output_filename, 'wb')
+        file = open(options.output_filename, 'w')
         file.write(lxml.etree.tostring(xmlrfc.tree.getroot(),
                                        xml_declaration=True,
                                        encoding='utf-8',
