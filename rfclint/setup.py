e56eda7c1947468b92f0c31bedc5c4f8254a0838
Jim Schaad <ietf@augustcellars.com>
2019-08-16 11:36:27 -0700

    Remove version 3.4
    3.4 is no longer supported by lxml so kill it for us as well.

--- rfclint/setup.py
+++ rfclint/setup.py
@@ -135,6 +135,7 @@ setup(
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         ],
 
     # What does your project relate to?
@@ -145,7 +146,7 @@ setup(
 
     # List run-time dependencies here.
     install_requires=requirements,
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, <4',
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4',
 
     # List additional groups of dependencies here.
     # extras_require=(
--- rfctools_common/setup.py
+++ rfctools_common/setup.py
@@ -100,7 +100,7 @@ setup(
         'Topic :: Text Processing :: Markup :: XML',
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
 
     # What does your project relate to?
@@ -111,7 +111,7 @@ setup(
 
     # List run-time dependencies here.
     install_requires=requirements,
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, <4',
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4',
 
     # List additional gorups of dependencies here.
     # extras_require=(
