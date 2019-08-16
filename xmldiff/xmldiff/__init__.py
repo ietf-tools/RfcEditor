0823592292fd9dc77582437f4a9bb9a8ddc16a66
Jim Schaad <ietf@augustcellars.com>
2019-08-16 11:56:37 -0700

    Update change logs
    Update the version numbers as necessary
    Update change logs to say 3.4 is no longer supported.

--- xmldiff/xmldiff/__init__.py
+++ xmldiff/xmldiff/__init__.py
@@ -3,7 +3,7 @@
 # ----------------------------------------------------
 
 # Static values
-__version__ = '0.5.16'
+__version__ = '0.5.17'
 NAME = 'xmldiff'
 VERSION = [int(i) if i.isdigit() else i for i in __version__.split('.')]
 
