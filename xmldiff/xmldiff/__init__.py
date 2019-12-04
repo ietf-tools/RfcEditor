bfae54c86299c3d0e742ed3d20c5cc9b09458cd6
Jim Schaad <ietf@augustcellars.com>
2019-12-04 11:52:56 -0800

    Fix two bugs in the text difference code
    THere were two bugs - one with prefix being totally used and one with the diagnal insert/replace code

--- xmldiff/xmldiff/__init__.py
+++ xmldiff/xmldiff/__init__.py
@@ -3,7 +3,7 @@
 # ----------------------------------------------------
 
 # Static values
-__version__ = '0.5.18'
+__version__ = '0.5.20'
 NAME = 'xmldiff'
 VERSION = [int(i) if i.isdigit() else i for i in __version__.split('.')]
 
